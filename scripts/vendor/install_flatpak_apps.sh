b7a4318c37d38548e5ee8ac29ddc91379fa64a61
Mathias SEGUIN-CADICHE <mathias.seguincadiche@gmail.com>
2026-08-16 03:27:20 -0400

    refactor: réserver Flatpak à Bitwarden

--- scripts/vendor/install_flatpak_apps.sh
+++ scripts/vendor/install_flatpak_apps.sh
@@ -6,4 +6,3 @@ command -v flatpak >/dev/null 2>&1
 
 flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --system -y flathub com.bitwarden.desktop
-flatpak install --system -y flathub org.onlyoffice.desktopeditors
